9c115ead51a15c13d59bf8b1d17eed1c7667a32b
ankitap1219 <156967746+ankitap1219@users.noreply.github.com>
2024-04-05 13:43:22 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "step-8" {
     ami = "ami-07761f3ae34c4478d"
     instance_type = "t2.micro"
-    key_name = "LINUX"
+    key_name = "SLAVE"
     vpc_security_group_ids = ["${aws_security_group.sg.id}"]
     subnet_id = aws_subnet.subnet-1.id
     associate_public_ip_address = true
